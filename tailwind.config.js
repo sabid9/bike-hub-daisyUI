b015fbbba5cb5aff1818369cc0255a6e336c5465
sabid9 <sabidjidan@gmail.com>
2024-10-21 12:19:52 +0600

    Bike Hub 1st commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  // ...
+}
